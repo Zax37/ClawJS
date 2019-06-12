dbf287dc9c6d043ced55edd01ed1bf47488ddea6
Zax37 <c37zax@gmail.com>
2019-06-12 22:57:24 +0200

    Rewrite process-palettes to be a standalone task.

--- tools/tasks/process-palettes.js
+++ tools/tasks/process-palettes.js
@@ -2,20 +2,21 @@ const fs = require('fs');
 const path = require('path');
 const paletteParser = require("../parsers/palette-parser");
 
-function processPalettes(targetArray) {
-  for (let i = 1; i <= 14; i++) {
-    const paletteFileName = path.join(__dirname, '../../resources/palettes', `LEVEL${i}.PAL`);
-    targetArray.push({
-      palette: paletteParser.parse(fs.readFileSync(paletteFileName)).data,
-      tileSets: ['ACTION', 'BACK', 'FRONT'].map(layerName => {
-        const fileName = path.join(__dirname, '../../resources/tilesets', `L${i}_${layerName}.json`);
-        return {
-          name: layerName,
-          mapping: JSON.parse(fs.readFileSync(fileName))
-        };
-      })
-    });
-  }
-}
+if (process.argv[2] && process.argv[3]) {
+  const inPath = path.resolve(process.argv[2]);
+  const outPath = path.resolve(process.argv[3]);
 
-module.exports = processPalettes;
+  const output = {};
+  for (let i = 1; i <= 14; i++) {
+    const key = 'LEVEL' + i;
+    const inputFilePath = path.resolve(inPath, key + '.PAL');
+    if (!fs.existsSync(inputFilePath)) {
+      console.log('Input file ' + inputFilePath + ' not found. Stopping palettes processing.');
+      break;
+    }
+    output[key] = { palette: paletteParser.parse(fs.readFileSync(inputFilePath)).data };
+  }
+  fs.writeFileSync(outPath, JSON.stringify(output));
+} else {
+  console.log("Call arguments: PALETTES_DIRECTORY_PATH, OUTPUT_FILE_PATH.")
+}
